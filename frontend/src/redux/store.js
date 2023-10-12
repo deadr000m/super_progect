c832c7542c86c43c34051ae6782743ad7b6d4f03
Roman Tishchenko <romkatisha@mail.ru>
2023-10-12 19:08:50 +0300

    Added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 // import booksReducer from './books/reducer';
 import filterReducer from './slices/filterSlice';
 import booksReducer from './slices/booksSlice';
+import errorReducer from './slices/errorSlice';
 
 const store = configureStore({
   reducer: {
     //тут будет объект, содержащий ридьюсеры
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 });
 
